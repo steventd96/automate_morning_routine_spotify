127ca7a861b23cd88e4af8b1d9a3dcf9028c8541
sdirjay <sdirjay@umich.edu>
2020-05-19 22:04:54 -0700

    Initial Commit

---
+++ script.py
@@ -0,0 +1,58 @@
+import spotipy
+import spotipy.util as util
+import json
+from datetime import datetime
+from random import random
+import variables as v
+
+def find_today_episode(date, podcastName):
+    searchResults = spotifyObject.search(podcastName,50,0,"episode")
+    while True:
+        for result in searchResults['episodes']['items']:
+            try:
+                if(result["release_date"] == date):
+                    #print(json.dumps(result, sort_keys=True, indent=4))
+                    return result["uri"]
+            except TypeError:
+                continue
+        if(searchResults['episodes']['next'] != None):
+            searchResults = spotifyObject.next(searchResults['episodes'])
+        else: 
+            print ("Error: Today's episode not found")
+
+def random_music(date):
+    searchResults = spotifyObject.current_user_playlists()
+    for result in searchResults["items"]:
+        if(result["name"] == v.playlistName):
+            randomNumber = (random() * 100) % 50
+            tracks = spotifyObject.playlist_tracks(result["id"])
+            return tracks["items"][int(randomNumber)]["track"]["uri"]
+
+
+    
+username = v.username
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+podcastNames = [v.podcastName1, v.podcastName2]
+
+token = util.prompt_for_user_token(username,
+                        scope,
+                        client_id= v.client_id,
+                        client_secret= v.client_secret,
+                        redirect_uri= v.redirect_uri
+                        )
+
+spotifyObject = spotipy.Spotify(auth=token)
+deviceID = spotifyObject.devices()['devices'][0]["id"]
+date = str(datetime.date(datetime.now()))
+
+uriList = []
+uri = random_music(date)
+uriList.append(uri)
+
+
+for name in podcastNames:
+    uri = find_today_episode(date,name)
+    uriList.append(uri)
+
+spotifyObject.start_playback(device_id = deviceID , uris = uriList)
+#spotifyObject.add_to_queue(,deviceID)
---
+++ variables_public.py
@@ -0,0 +1,7 @@
+username = "Enter Spotify User ID"
+client_id = "Enter Client ID"
+client_secret = "Enter Client Secret"
+redirect_uri = "Enter redirect_uri"
+podcastName1 = "Enter first podcast title"
+podcastName2 = "Enter second podcast title"
+playlistName = "Enter the name of your own playlist"
